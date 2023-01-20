391d8e86ec95648e0f0fbddc71a34091dc051c3c
Pingvu <nhlpool27@gmail.com>
2023-01-20 13:48:21 +0100

    Add project files.

---
+++ Övning 5.1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Hur många mening vill du skriva?");
+int Tot = int.Parse(Console.ReadLine());
+string[] N = new string[Tot];
+for (int i = 0; i < Tot; i++)
+{
+    Console.WriteLine("Skriv en mening");
+    N[i] = Console.ReadLine();
+}
+for (int i = 0; i < Tot; i++)
+{
+    Console.WriteLine(N[i]);
+    Console.WriteLine();
+}
